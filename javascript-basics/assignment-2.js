208935e35d69a70d44dc60fb2030a39b85745e78
Chantal Jenkins <norwegimo@icloud.com>
2018-06-03 12:32:59 -0700

    Update assignment 2 with 2nd Challenge

--- javascript-basics/assignment-2.js
+++ javascript-basics/assignment-2.js
@@ -1,3 +1,5 @@
+//Challenge #1
+
 // When I first set out to solve this assignement I came up with the following:
 // I thought I would try a do loop as it was certain that the first console log was "#" 
 // I quickly realized that this printed out the actual array and not just the content.
@@ -36,3 +38,30 @@ for (var i = 1; i <= 7; i++ ) {
   h = h + "#";
 }
 
+
+// Challenge #2
+
+function isEven(num) {
+  if (num % 2  == 0) {
+    console.log("The number is even")
+  } else if (num % 2 > 0) {
+    console.log("This number is odd")
+  } else {
+    console.log("This is not a number")
+  }
+}
+
+isEven(1);
+isEven(2);
+isEven("a");
+
+// Decided to use an if/else statement.  
+// 1. Using first the modulo operator to decipher if the num provided is even.  It will divide by 2
+//    and if the remaining is 0 this will be a true statement and
+//    this will prompt the console log to print "The number is even"
+// 2. If the first if statement is false it will then go to the else if 
+//    with this modulo operator anything with a remainder of anything
+//    above zero will be consider true statement and the console.log 
+//    will print "This number is odd"
+// 3. If the else if statement is false it will go to the else where 
+//    everything else that is not a numbr will log ("This is not a number").
